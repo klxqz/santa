030259eae6b8a2e435361bade34683ad2efa35f1
klxqz <klxqz@mail.ru>
2013-11-26 15:23:33 +0600

    новая версия

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -3,11 +3,11 @@
 return array(
     'name' => 'Дед мороз',
     'description' => 'Анимированный дед мороз на вашем сайте',
-    'vendor'=>903438,
-    'version'=>'1.0.0',
-    'img'=>'img/santa.png',
+    'vendor' => '985310',
+    'version' => '1.0.1',
+    'img' => 'img/santa.png',
     'shop_settings' => true,
-    'frontend'    => true,
+    'frontend' => true,
     'handlers' => array(
         'frontend_head' => 'frontendHead',
     ),
